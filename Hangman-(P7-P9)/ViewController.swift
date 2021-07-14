469bc679197b7f34716336fd2b8c124785c9cfb7
Łukasz Nycz <lnycz96@gmail.com>
2021-07-14 15:03:43 +0200

    Initial Commit

---
+++ Hangman-(P7-P9)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hangman-(P7-P9)
+//
+//  Created by Łukasz Nycz on 14/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
