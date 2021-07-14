6916306ceb82724ad017cf708c333029893f66c8
Łukasz Nycz <lnycz96@gmail.com>
2021-07-14 16:06:49 +0200

    Building interface

--- Hangman-(P7-P9)/ViewController.swift
+++ Hangman-(P7-P9)/ViewController.swift
@@ -9,9 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var letterButtons = [UIButton]()
+    @IBOutlet var letterView: UIView!
+    @IBOutlet var currentAnswer: UITextField!
+    var activatedButtons = [UIButton]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let buttonView = UIView()
+        
+        let width = 150
+        let height = 80
+        
+        for row in 0..<4 {
+            for column in 0..<5 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                letterButton.setTitle("WWW", for: .normal)
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+                
+                let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                letterView.addSubview(letterButton)
+                
+                letterButtons.append(letterButton)
+            }
+        }
+    }
+    
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
 
 
